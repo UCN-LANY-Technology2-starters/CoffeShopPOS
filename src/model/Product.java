135812ce0a1e32c73856a7cb3d6a8131fd633912
Lars Nysom <LANY@ucn.dk>
2023-01-11 22:46:02 +0100

    Update Product.java
    changed datatype on parameter in constructor

--- src/model/Product.java
+++ src/model/Product.java
@@ -18,7 +18,7 @@ public class Product {
 		return price;
 	}
 	
-	public Product(String name, String description, int price) {
+	public Product(String name, String description, double price) {
 		this.name = name;
 		this.description = description;
 		this.price = price;
